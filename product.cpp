4a9bc68e388ed08ced6b36356b93ab77e63eab50
Jake Herrmann <jtherrmann@alaska.edu>
2018-09-10 23:04:28 -0800

    Finish Project 1 (class Product).

--- product.cpp
+++ product.cpp
@@ -1,44 +1,51 @@
-#include "product.h"
+// product.cpp
+// Jake Herrmann
+// 11 Sep 2018
+//
+// CS 311 Fall 2018
+// Source for class Product
 
-std::string Product::getName() const {
-    return _name;
+#include "product.h"  // class Product
+#include <string>  // std::string, std::to_string
+#include <ostream>  // std::ostream
+
+
+// ============================================================================
+// class Product: Definitions of ctors.
+// ============================================================================
+
+// Default ctor.
+Product::Product(): _name(""), _items(0) {}
+
+// Two-parameter ctor.
+Product::Product(std::string name, int items) {
+    setName(name);
+    setInventory(items);
 }
 
-int Product::getInventory() const {
-    return _items;
-}
 
-void Product::setName(std::string name) {
-    _name = name;
-}
+// ============================================================================
+// class Product: Definitions of public operators.
+// ============================================================================
 
-void Product::setInventory(int items) {
-    _items = items;
-}
-
-bool Product::empty() const {
-    return getInventory() == 0;
-}
-
-std::string Product::toString() const {
-    return getName() + " (" + std::to_string(getInventory()) + ")";
-}
-
+// op==
 bool Product::operator==(const Product & other) const {
-    return getName() == other.getName() && getInventory() == other.getInventory();
+    return (getName() == other.getName()
+	    && getInventory() == other.getInventory());
 }
 
+// op!=
 bool Product::operator!=(const Product & other) const {
     return !(*this == other);
 }
 
-// TODO: pre
+// op++ (pre)
 Product & Product::operator++() {
     ++_items;
     return *this;
 }
 
-// TODO: post
+// op++ (post)
 Product Product::operator++(int dummy) {
     (void)dummy;
     auto copy = *this;
@@ -46,14 +53,14 @@ Product Product::operator++(int dummy) {
     return copy;
 }
 
-// TODO: pre
+// op-- (pre)
 Product & Product::operator--() {
     if (_items > 0)
 	--_items;
     return *this;
 }
 
-// TODO: post
+// op-- (post)
 Product Product::operator--(int dummy) {
     (void)dummy;
     auto copy = *this;
@@ -61,6 +68,47 @@ Product Product::operator--(int dummy) {
     return copy;
 }
 
+
+// ============================================================================
+// class Product: Definitions of member functions.
+// ============================================================================
+
+// getName
+std::string Product::getName() const {
+    return _name;
+}
+
+// getInventory
+int Product::getInventory() const {
+    return _items;
+}
+
+// setName
+void Product::setName(std::string name) {
+    _name = name;
+}
+
+// setInventory
+void Product::setInventory(int items) {
+    _items = items;
+}
+
+// empty
+bool Product::empty() const {
+    return getInventory() == 0;
+}
+
+// toString
+std::string Product::toString() const {
+    return getName() + " (" + std::to_string(getInventory()) + ")";
+}
+
+
+// ============================================================================
+// class Product: Definitions of associated global operators.
+// ============================================================================
+
+// op<<
 std::ostream & operator<<(std::ostream & out, const Product & p) {
     out << p.toString();
     return out;
--- product.h
+++ product.h
@@ -1,73 +1,123 @@
 // product.h
 // Jake Herrmann
-// CS 311 Fall 2018
 // 11 Sep 2018
+//
+// CS 311 Fall 2018
+// Header for class Product
 
 #ifndef FILE_PRODUCT_H_INCLUDED
 #define FILE_PRODUCT_H_INCLUDED
 
-// TODO: include string again in header file and move to_string comment to there?
-#include <string>  // std::string, std::to_string
-
-// TODO: include ostream again in header file?
+#include <string>  // std::string
 #include <ostream>  // std::ostream
 
+
+// ============================================================================
+// class Product: Class definition.
+// ============================================================================
+
+// class Product
+// A product: name and number of items in inventory.
+// Invariants:
+//     _items >= 0
 class Product {
 
+// Product: Ctors and the Big Five.
 public:
 
-    // TODO: implement constructors in product.cpp instead?
-    // TODO: use 'this' for clarity?
+    // Default ctor.
+    // Set name to "" and number of items to 0.
+    Product();
 
-    Product(): _name(""), _items(0) {}
+    // Ctor from name and number of items.
+    // Pre:
+    //     items >= 0
+    Product(std::string name, int items);
 
-    // TODO: Project 1 "Other Requirements"; # of items cannot be negative;
-    // class invariant, or pre-condition here?
-    Product(std::string name, int items) {
-    	setName(name);
-    	setInventory(items);
-    }
-
+    // Use compiler-generated versions of the Big Five.
     ~Product() = default;
     Product(const Product & other) = default;
     Product(Product && other) = default;
     Product & operator=(const Product & other) = default;
     Product & operator=(Product && other) = default;
 
-    std::string getName() const;
-    int getInventory() const;
+// Product: Public operators.
+public:
 
-    void setName(std::string name);
-    void setInventory(int items);
-
-    bool empty() const;
-
-    std::string toString() const;
-
+    // op==
+    // Compare the two products' names and number of items. Return true if
+    // equal and false otherwise.
     bool operator==(const Product & other) const;
+
+    // op!=
+    // Compare the two products' names and number of items. Return true if not
+    // equal and false otherwise.
     bool operator!=(const Product & other) const;
 
-    // TODO: pre
+    // op++ (pre)
+    // Increase number of items by one.
     Product & operator++();
 
-    // TODO: post
+    // op++ (post)
+    // Increase number of items by one.
     Product operator++(int dummy);
 
-    // TODO: pre
+    // op-- (pre)
+    // Decrease number of items by one.
     Product & operator--();
 
-    // TODO: post
+    // op-- (post)
+    // Decrease number of items by one.
     Product operator--(int dummy);
-	
 
+// Product: Member functions.
+public:
+
+    // getName
+    // Return the product's name.
+    std::string getName() const;
+
+    // getInventory
+    // Return the number of items in the inventory.
+    int getInventory() const;
+
+    // setName
+    // Set the product's name.
+    void setName(std::string name);
+
+    // setInventory
+    // Set the number of items in the inventory.
+    // Pre:
+    //     items >= 0
+    void setInventory(int items);
+
+    // empty
+    // Return true if the number of items is zero and false otherwise.
+    bool empty() const;
+
+    // toString
+    // Return a string representation of the product's name and number of
+    // items.
+    std::string toString() const;
+
+// Product: Data members.
 private:
 
+    // The product's name.
     std::string _name;
+
+    // Number of items in inventory.
     int _items;
 
 };  // End class Product.
 
 
+// ============================================================================
+// class Product: Associated global operators.
+// ============================================================================
+
+// op<<
+// Print the product's name and number of items.
 std::ostream & operator<<(std::ostream & out, const Product & p);
 
 
