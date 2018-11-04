905643a3397ea0abf4ce3187452877610b9d9801
Jake Herrmann <jtherrmann@alaska.edu>
2018-11-03 16:18:56 -0800

    Make project 6 tests pass for class template ListMap.

--- project6/da6.h
+++ project6/da6.h
@@ -14,13 +14,14 @@
 // For std::size_t
 
 #include <memory>
-// For std::shared_ptr
+// For std::shared_ptr, std::make_shared
 
 #include <utility>
-// For std::pair
+// For std::pair, std::make_pair
 
 
 #include "llnode2.h"
+// For LLNode2
 
 
 template<typename ValType>
@@ -43,11 +44,14 @@ public:
     // TODO: acceptable solution for kv_type?
     using kv_type = std::pair<KType, VType>;
 
+    using node_type = LLNode2<kv_type>;
+
 public:
 
     // Default ctor
-    // TODO
-    ListMap() {}
+    ListMap()
+	:_head()
+    {}
 
     // Dctor
     // TODO; or default?
@@ -60,43 +64,96 @@ public:
 
 public:
 
+    // TODO: confirm not allowed to store size as data member
     size_type size() const {
-	// TODO
-	return 0;
+	int count = 0;
+	auto _current = _head;
+	while (_current) {
+	    ++count;
+	    _current = _current->_next;
+	}
+	return count;
     }
 
     bool empty() const {
-	//TODO
-	return true;
+	return !_head;
     }
 
-    // TODO: how to make const version?
+    // TODO: factor out search method
     // TODO: param passing method?
-    value_type * find(key_type key) const {
-	//TODO
+    value_type * find(key_type key) {
+	auto _current = _head;
+	while (_current) {
+	    if (_current->_data.first == key)
+		return &(_current->_data.second);
+	    _current = _current->_next;
+	}
+	return nullptr;
+    }
+    const value_type * find(key_type key) const {
+	auto _current = _head;
+	while (_current) {
+	    if (_current->_data.first == key)
+		return &(_current->_data.second);
+	    _current = _current->_next;
+	}
 	return nullptr;
     }
 
+    // TODO: factor out search method
     // TODO: param passing methods?
     void insert(key_type key, value_type value) {
-	//TODO
+	auto _current = _head;
+	bool done = false;
+	while (_current && !done) {
+	    if (_current->_data.first == key) {
+		_current->_data.second = value;
+		done = true;
+	    }
+	    _current = _current->_next;
+	}
+	if (!done) {
+	    auto newpair = std::make_pair(key, value);
+	    auto newnode = node_type(newpair, _head);
+	    _head = std::make_shared<node_type>(newnode);
+	}
     }
 
+    // TODO: factor out search method
     // TODO: param passing methods?
     void erase(key_type key) {
-	//TODO
+	// TODO: how to free memory of removed node? or is that handled by
+	// shared_ptr?
+	if (!empty()) {
+	    if (_head->_data.first == key)
+		_head = _head->_next;
+	    else {
+	    	auto _current = _head;
+	    	while (_current->_next) {
+	    	    if (_current->_next->_data.first == key) {
+	    	    	_current->_next = _current->_next->_next;
+			break;
+		    }
+		    _current = _current->_next;
+	    	}
+	    }
+	}
     }
 
     template <typename Func>
     void traverse(Func f) {
-	//TODO
+	auto _current = _head;
+	while (_current) {
+	    f(_current->_data.first, _current->_data.second);
+	    _current = _current->_next;
+	}
     }
 
 
 
 private:
 
-    std::shared_ptr<LLNode2<kv_type>> _head;  // TODO: appropriate name?
+    std::shared_ptr<node_type> _head;
 
 };  // End class template ListMap
 
