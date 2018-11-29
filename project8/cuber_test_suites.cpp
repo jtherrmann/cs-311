a8b5e4fb6846bd0bf637187ad5578f8dc3718e24
Jake Herrmann <jtherrmann@alaska.edu>
2018-11-29 10:17:15 -0900

    Add cuber.h and cuber_test_suites.cpp for project 8.

---
+++ project8/cuber_test_suites.cpp
@@ -0,0 +1,33 @@
+// TODO toplevel docs, credit chappell
+
+// cuber_test_suites.cpp  INCOMPLETE
+// Glenn G. Chappell
+// 27 Nov 2018
+//
+// For CS 311 Fall 2018
+// Tests for class Cuber: test suites
+// For Project 8, Exercise B
+// Uses the "Catch" unit-testing framework, version 2
+// Requires cuber_test_main.cpp, catch.hpp, cuber.h
+
+// Includes for code to be tested
+#include "cuber.h"         // For class Cuber
+#include "cuber.h"         // Double inclusion test
+
+#define CATCH_CONFIG_FAST_COMPILE
+                             // Disable some features for faster compile
+#include "catch.hpp"         // For the "Catch" unit-testing framework
+
+// Additional includes for this test program
+
+
+// *********************************************************************
+// Test Cases
+// *********************************************************************
+
+
+TEST_CASE("DUMMY")
+{
+    // DUMMY TEST CASE
+}
+
