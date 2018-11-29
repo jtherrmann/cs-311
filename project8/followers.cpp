cb8460f69f590ed241492949626c7f89e9892b00
Jake Herrmann <jtherrmann@alaska.edu>
2018-11-29 09:58:12 -0900

    Mostly finish project 8 exercise A (following-words program).

--- project8/followers.cpp
+++ project8/followers.cpp
@@ -6,6 +6,12 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#include <map>
+using std::map;
+
+#include <set>
+using std::set;
+
 #include <string>
 using std:: getline;
 using std::string;
@@ -14,6 +20,66 @@ using std::string;
 // TODO:
 // - address TODO in file
 // - remove sources marked TODO
+// - document
+// - read project description & coding standards
+
+
+// TODO:
+// https://www.cs.uaf.edu/~chappell/class/2018_fall/cs311/lect/cs311-20181126-table_stl.pdf
+// slide 26
+// - use std::map from <map>
+//   - because keys are unique and sorted by operator<
+// - for each word, if it's not a key in the map, set it to an empty set
+// - for each word, insert the word immediately after it into the first word's
+//   set
+
+
+using SetType = set<string>;
+using MapType = map<string, SetType>;
+
+
+MapType & make_word_map(const string & filename, MapType & wordmap)
+{
+    // TODO: source: http://www.cplusplus.com/doc/tutorial/files/
+    string current_word;
+    string next_word;
+    ifstream file;
+
+    file.open(filename);
+    file >> next_word;
+    if (file) {
+	while (true) {
+	    current_word = next_word;
+
+	    if (wordmap.count(current_word) == 0)
+		wordmap[current_word] = SetType();
+
+	    file >> next_word;
+	    if (!file)
+		break;
+
+	    wordmap[current_word].insert(next_word);
+	}
+    }
+    file.close();
+    if (!file.eof()) {
+	cout << "Error reading file '" << filename << "'" << endl;
+	exit(1);
+    }
+    return wordmap;
+}
+
+
+void print_stats(const MapType & wordmap)
+{
+    cout << "\nNumber of distinct words: " << wordmap.size() << "\n\n";
+    for (const auto & kvpair : wordmap) {
+	cout << kvpair.first << ":";
+	for (const auto & word : kvpair.second)
+	    cout << " " << word;
+	cout << endl;
+    }
+}
 
 
 int main()
@@ -23,27 +89,15 @@ int main()
     //   - trim whitespace from beginnig & end of input filename
 
     // TODO: source: https://en.cppreference.com/w/cpp/string/basic_string/getline
+
     string filename;
+    MapType wordmap;
+
     cout << "Enter a filename: ";
     getline(cin, filename);
 
-    int total = 0; // TODO temp
-    string word;
-    ifstream file;
-    file.open(filename);
-    while (true) {
-	file >> word;
-	if (!file)
-	    break;
-	++total;
-	cout << word << " ";  // TODO temp
-    }
-    file.close();
-    if (!file.eof())
-	cout << "Error reading file '" << filename << "'" << endl;
-    else
-	// TODO: temp
-	cout << total << endl;
+    make_word_map(filename, wordmap);
+    print_stats(wordmap);
 
     return 0;
 }
